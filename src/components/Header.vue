40edac179209afae8177b8c30bd5d9797f9caa39
Gut Mateusz <gutmateusz96@interia.pl>
2020-04-09 13:53:06 +0200

    Added base routing and navbar with collapse

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand>
+        <router-link to="/" class="navbar-brand">
+          <a>Quiz App</a>
+        </router-link>
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link to="/" activeClass="active" router-tag="li" exact>
+              <a class="nav-link">Home</a>
+            </router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/quiz-menu" activeClass="active" router-tag="li">
+              <a class="nav-link">Quiz</a>
+            </router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/players" activeClass="active" router-tag="li">
+              <a class="nav-link">Players</a>
+            </router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/stats" activeClass="active" router-tag="li">
+              <a class="nav-link">Stats</a>
+            </router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link
+              to="/question-creator"
+              class="nav-item"
+              activeClass="active"
+              router-tag="li"
+            >
+              <a class="nav-link">Question Creator</a>
+            </router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+</style>
