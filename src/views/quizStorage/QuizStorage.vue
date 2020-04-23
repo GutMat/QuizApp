c445a9b65a9d6218f15fbca173b3c3b8969b98ed
Gut Mateusz <gutmateusz96@interia.pl>
2020-04-23 18:45:19 +0200

    Added appQuiz to quizStorage, upgraded the form structure

--- src/views/quizStorage/QuizStorage.vue
+++ src/views/quizStorage/QuizStorage.vue
@@ -10,32 +10,62 @@
     </div>
     <div v-if="isTriviaQuizVisible">
       <h3>Trivia</h3>
-      <h4>Choose category</h4>
+      <h5>Choose category</h5>
       <div>
         <select v-model="selectedCategory">
           <option
             v-for="(category, index) in triviaCategories[0]"
-            :value="category.name"
+            :value="category.id"
             :key="index"
             >{{ category.name }}</option
           >
         </select>
         <p>{{ selectedCategory }}</p>
+        <select v-model="selectedDifficulty">
+          <option value="">Any Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+        <p>
+          {{ selectedDifficulty == "" ? "Any Difficulty" : selectedDifficulty }}
+        </p>
+        <h5>Number of questions</h5>
+        <input type="number" min="1" max="50" v-model.number="questionAmount" />
+        <p>{{ questionAmount }}</p>
+      </div>
+      <button @click="displayQuiz">Display quiz</button>
+      <div v-if="isQuizVisible">
+        <AppQuiz
+          v-for="(question, index) in trivia[1]"
+          :key="index"
+          :question="question.question"
+          :correct="question.correct_answer"
+          :incorrect="question.incorrect_answers"
+        ></AppQuiz>
       </div>
       <button @click="goBack">Back</button>
     </div>
   </div>
 </template>
 <script>
+import AppQuiz from "../../components/quiz/Quiz.vue";
 export default {
   data() {
     return {
       isMenuVisible: true,
       isCountryQuizVisible: false,
       isTriviaQuizVisible: false,
+      isQuizVisible: false,
       triviaCategories: [],
       selectedCategory: "",
+      questionAmount: 1,
+      selectedDifficulty: "",
+      trivia: [],
     };
+  },
+  components: {
+    AppQuiz,
   },
   methods: {
     displayTrivia() {
@@ -49,10 +79,11 @@ export default {
           for (let key in data) {
             categories.push(data[key]);
           }
+          console.log(categories);
           this.triviaCategories = categories;
+          this.isTriviaQuizVisible = true;
+          this.isMenuVisible = false;
         });
-      this.isTriviaQuizVisible = true;
-      this.isMenuVisible = false;
     },
     displayFlagQuiz() {
       this.isCountryQuizVisible = true;
@@ -63,6 +94,30 @@ export default {
       this.isCountryQuizVisible = false;
       this.isMenuVisible = true;
       this.selectedCategory = "";
+    },
+    displayQuiz() {
+      this.$http
+        .get(
+          "https://opentdb.com/api.php?amount=" +
+            this.questionAmount +
+            "&category=" +
+            this.selectedCategory +
+            "&difficulty=" +
+            this.selectedDifficulty +
+            "&type=multiple"
+        )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const questions = [];
+          for (let key in data) {
+            questions.push(data[key]);
+          }
+          console.log(questions);
+          this.trivia = questions;
+          this.isQuizVisible = true;
+        });
     },
   },
 };
