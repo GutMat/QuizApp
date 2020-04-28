12e5e66ab448720e06f62e9fefc27e9e413e8704
Gut Mateusz <gutmateusz96@interia.pl>
2020-04-28 12:57:19 +0200

    Added base question creator structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    id="app"
-    style="backgroundImage: url('https://scontent-frt3-1.xx.fbcdn.net/v/t1.15752-9/94129989_3774803345894149_7669253239082057728_n.png?_nc_cat=104&_nc_sid=b96e70&_nc_oc=AQljDqk-dM21h2q-7AxVgXbmCONpVEtGxGQqyXbDUfhhlPbv405g5hQ-13hwTHVpe6k&_nc_ht=scontent-frt3-1.xx&oh=d6c81c9849de2b967896dcb7d4f55575&oe=5EC83BB5'); height: 900px;"
-  >
+  <div id="app">
     <AppHeader></AppHeader>
     <div class="container-fluid">
       <router-view />
@@ -13,9 +10,8 @@
 import AppHeader from "./components/shared/Header.vue";
 export default {
   components: {
-    AppHeader
-  }
+    AppHeader,
+  },
 };
 </script>
-<style>
-</style>
+<style></style>
--- src/views/questionCreator/QuestionCreator.vue
+++ src/views/questionCreator/QuestionCreator.vue
@@ -1,13 +1,66 @@
 <template>
-    <div>
-        <h1>Question Creator Component</h1>
+  <div>
+    <button @click="isQuestionFormVisible = !isQuestionFormVisible">
+      Add Question
+    </button>
+    <button @click="isQuestionManagementVisible = !isQuestionManagementVisible">
+      Manage Question
+    </button>
+    <div v-if="isQuestionFormVisible">
+      <form v-on:submit.prevent>
+        <label>Question:</label>
+        <input type="text" v-model="question" /><br /><br />
+        <p>{{ question }}</p>
+        <label>Correct Answer:</label>
+        <input type="text" v-model="answers.correctAnswer" /><br /><br />
+        <label>Incorrect Answer 1:</label>
+        <input type="text" v-model="incorrectAnswerFirst" /><br /><br />
+        <label>Incorrect Answer 2:</label>
+        <input type="text" v-model="incorrectAnswerSecond" /><br /><br />
+        <label>Incorrect Answer 3:</label>
+        <input type="text" v-model="incorrectAnswerThird" /><br /><br />
+        <p>{{ answers }}</p>
+        <button @click="saveQuestion">Add question</button>
+      </form>
+      <p>{{ questions }}</p>
     </div>
+    <div v-if="isQuestionManagementVisible">Temp Question List</div>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      questions: [],
+      question: "",
+      incorrectAnswerFirst: "",
+      incorrectAnswerSecond: "",
+      incorrectAnswerThird: "",
+      answers: {
+        correctAnswer: "",
+        incorrectAnswers: [],
+      },
+      isQuestionFormVisible: false,
+      isQuestionManagementVisible: false,
+    };
+  },
+  methods: {
+    saveQuestion() {
+      let tempIncorrectAnswers = [
+        this.incorrectAnswerFirst,
+        this.incorrectAnswerSecond,
+        this.incorrectAnswerThird,
+      ];
+      this.answers.incorrectAnswers = tempIncorrectAnswers;
+      let tempQuestions = this.questions;
+      tempQuestions.push({
+        question: this.question,
+        correct: this.answers.correctAnswer,
+        incorrect: this.answers.incorrectAnswers,
+      });
+      console.log(this.questions);
+    },
+  },
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
